4762d4a266fbb71d8ec9bdb2a3651dffb3c0760d
dhanusho9 <dhanushkumar4803@gmail.com>
2026-02-14 18:43:34 +0530

    initial commit

---
+++ src/main/java/com/alpha/FoodDeliveryExpress/FoodDeliveryExpressApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.FoodDeliveryExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/FoodDeliveryExpress/FoodDeliveryExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.FoodDeliveryExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
